58d558f035fe6271701e2c87f839e267d96ae40d
Rubén Díaz <44802020+TheMilubi@users.noreply.github.com>
2021-02-26 20:00:56 +0000

    Update webdriver.py

--- webdriver.py
+++ webdriver.py
@@ -30,7 +30,7 @@ def ObtenerDatosGrupoGoogleCSV(html):
     posicion = soup.select('h1.KdPHLc')[0].string
     
     members = soup.select("div[role='gridcell'] a[href^='mailto:']")
-    s = "First Name,Last Name,Email,Position"
+    s = "First Name,Last Name,Email,Position\n"
     for m in members:
         if '@alu' not in m.string and '@fp' not in m.string:
             continue
